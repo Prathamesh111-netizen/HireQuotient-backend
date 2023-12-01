e6e169d2997adf15fc1e9404f203ffbc6b5c3d92
Prathamesh Pawar <79742694+Prathamesh111-netizen@users.noreply.github.com>
2023-12-02 05:24:54 +0530

    initial setup with users Model creation and MongoDB setup

---
+++ index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+dotenv.config();
+
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+const app = express();
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(express.json({ limit: "5mb" })); // middleware to use req.body
+
+app.use(cors()); // to avoid CORS errors
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// middleware to act as fallback for all 404 errors
+app.use(notFound);
+
+// configure a custome error handler middleware
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+});
